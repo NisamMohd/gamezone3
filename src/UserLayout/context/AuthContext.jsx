cb1c0a58b2639ed11ae693a62eb2ee26cf380edf
NisamMohd <nisammohd514@gmail.com>
2026-08-03 18:38:21 +0530

    AuthContext created and register functionality added

---
+++ src/UserLayout/context/AuthContext.jsx
@@ -0,0 +1,64 @@
+import { createContext, useContext, useState } from "react";
+import { isUserExist } from "../utils/context";
+import api from "../services/api";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const [user, setUser] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("user")) || null;
+    } catch {
+      return null;
+    }
+  });
+
+  const register = async (userinfo) => {
+    setIsLoading(true);
+
+    try {
+      const isExisting = await isUserExist(userinfo.email);
+      if (isExisting.userExist) {
+        throw new Error(isExisting.message);
+      }
+
+      const res = await api.post(`/users`, {
+        ...userinfo,
+        createdAt: new Date().toISOString(),
+        role: "user",
+      });
+
+      
+        setUser(res.data);
+        localStorage.setItem("user", JSON.stringify(res.data));
+
+        return res.data
+      
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        error,
+        setError,
+        isLoading,
+        register,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
