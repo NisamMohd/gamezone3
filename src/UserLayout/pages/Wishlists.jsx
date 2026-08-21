77d58f858e7105b8016cb20214f494cbd9fd5d3e
NisamMohd <nisammohd514@gmail.com>
2026-08-21 15:56:04 +0530

    wishlist page and redux setups initialised

--- src/UserLayout/pages/Wishlists.jsx
+++ src/UserLayout/pages/Wishlists.jsx
@@ -2,26 +2,7 @@ import React from 'react'
 
 function Wishlists() {
   return (
-    <div>
-      {/* {items.map((item) => (
-            <div> 
-                <div>
-                    <img src={item.img} alt="" />
-                </div>
-                <div>
-                    <h2>{item.title}</h2>
-                    <hr />
-                    <div>
-                        {item.description}
-                    </div>
-                </div> */}
-                <div>
-                    <button>Remove</button>
-                </div>
-            {/* </div>
-        ))} */}
-    </div>
-    
+    <div>Wishlists</div>
   )
 }
 
