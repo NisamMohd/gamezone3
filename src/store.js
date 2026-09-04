0f736b6b44d2cad6125a3addd3e9d7c1a747400e
NisamMohd <nisammohd514@gmail.com>
2026-09-04 14:32:29 +0530

    store is moved from UserLayout/redux/store.js to /src/store.js

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from './UserLayout/redux/features/productSlice'
+import cartReducer from './UserLayout/redux/features/cartSlice'
+import wishlistReducer from './UserLayout/redux/features/wishlistSlice'
+import orderReducer from './UserLayout/redux/features/orderSlice'
+
+export const store = configureStore({
+    reducer: {
+        products: productReducer,
+        cart: cartReducer,
+        wishlist: wishlistReducer,
+        orders: orderReducer,
+    }
+})
