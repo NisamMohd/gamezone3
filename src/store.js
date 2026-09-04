3f755b2d2b3b9d9df818cc4d1c20b1ec8f313ce1
NisamMohd <nisammohd514@gmail.com>
2026-09-04 17:32:58 +0530

    authSlice added to store

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import productReducer from './UserLayout/redux/features/productSlice'
 import cartReducer from './UserLayout/redux/features/cartSlice'
 import wishlistReducer from './UserLayout/redux/features/wishlistSlice'
 import orderReducer from './UserLayout/redux/features/orderSlice'
+import authReducer from './features/authSlice'
 
 export const store = configureStore({
     reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
         cart: cartReducer,
         wishlist: wishlistReducer,
         orders: orderReducer,
+        auth: authReducer
     }
 })
