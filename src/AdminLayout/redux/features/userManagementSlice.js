04d0ed53f52b1cdf13a0a7c631465c5d9158aabd
NisamMohd <nisammohd514@gmail.com>
2026-09-14 02:42:00 +0530

    addCase for toggleblockuser added

--- src/AdminLayout/redux/features/userManagementSlice.js
+++ src/AdminLayout/redux/features/userManagementSlice.js
@@ -1,40 +1,50 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { customerList } from "../thunks/customerThunk";
-
+import { toggleBlockuser } from "../thunks/blockuserThunk";
 
 const userManagementSlice = createSlice({
-    name : 'users',
-    initialState : {
-        items : [],
-        status : 'idle',
-        error : null
-    },
-    reducers : {},
-    extraReducers : (builder) => {
-        builder
-        // Customers
-            .addCase(customerList.fulfilled, ( state, action) => {
-                state.status = 'succeeded';
-                state.items = action.payload;
-            })
-        
-        
-        // handles pending and rejected actions
-            .addMatcher(
-            (action) => action.type.startsWith("users/") && action.type.endsWith("/pending"),
-            (state) => {
-                state.status = 'loading';
-                state.error = null;
-            })
+  name: "users",
+  initialState: {
+    items: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // Customers
+      .addCase(customerList.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
 
-            .addMatcher(
-                (action) => action.type.startsWith("users/") && action.type.endsWith("/rejected"),
-                (state, action) => {
-                    state.status = 'failed';
-                    state.error = action.payload;
-                }
-            )
-    }
-})
+      //Block Usere
+      .addCase(toggleBlockuser.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = state.items.map((item) =>
+          String(item.id) === String(action.payload.id) ? action.payload : item,
+        );
+      })
+
+      // handles pending and rejected actions
+      .addMatcher(
+        (action) =>
+          action.type.startsWith("users/") && action.type.endsWith("/pending"),
+        (state) => {
+          state.status = "loading";
+          state.error = null;
+        },
+      )
+
+      .addMatcher(
+        (action) =>
+          action.type.startsWith("users/") && action.type.endsWith("/rejected"),
+        (state, action) => {
+          state.status = "failed";
+          state.error = action.payload;
+        },
+      );
+  },
+});
 
 export default userManagementSlice.reducer;
