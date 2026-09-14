28646f18830a90a6cdfc81d3dc346372fee9a81c
NisamMohd <nisammohd514@gmail.com>
2026-09-15 00:55:59 +0530

    toggleisDisabledthunk setup

--- src/AdminLayout/redux/features/ProductManageSlice.js
+++ src/AdminLayout/redux/features/ProductManageSlice.js
@@ -1,39 +1,53 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchProducts } from "../thunks/adminProductsThunk";
+import { toggleDisable } from "../thunks/toggleIsDisabledThunk";
 
 const ProductManageSlice = createSlice({
-    name : "adminProducts",
-    initialState : {
-        items : [],
-        status: "idle",
-        error : null
-    },
-    reducers : {},
-    extraReducers : (builder) => {
-        builder
-        // FETCH PRODUCTS
-        .addCase(fetchProducts.fulfilled, (state, action) => {
-            state.status = "succeeded";
-            state.items = action.payload;
-        })
+  name: "adminProducts",
+  initialState: {
+    items: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // FETCH PRODUCTS
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
 
-        // HANDLE PENDING STATE
-        .addMatcher(
-            (action) => 
-                action.type.startsWith("adminProducts/") && action.type.endsWith("/pending"),
-            (state) => {state.status = "loading"}
-        )
+      //TOGGLE DISABLE
+      .addCase(toggleDisable.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = state.items.map((product) =>
+          String(product.id) === String(action.payload.id)
+            ? action.payload
+            : product,
+        );
+      })
 
-        .addMatcher(
-            (action) => 
-                action.type.startsWith("adminProducts/") && action.type.endsWith("/rejected"),
-            (state, action) => {
-                state.status = "failed";
-                state.error = action.payload;
-            }
-        )
-    }
+      // HANDLE PENDING STATE
+      .addMatcher(
+        (action) =>
+          action.type.startsWith("adminProducts/") &&
+          action.type.endsWith("/pending"),
+        (state) => {
+          state.status = "loading";
+        },
+      )
 
-})
+      .addMatcher(
+        (action) =>
+          action.type.startsWith("adminProducts/") &&
+          action.type.endsWith("/rejected"),
+        (state, action) => {
+          state.status = "failed";
+          state.error = action.payload;
+        },
+      );
+  },
+});
 
 export default ProductManageSlice.reducer;
