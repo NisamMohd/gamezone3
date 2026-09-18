815938dd6819257342d5df856ec85bed787065c1
NisamMohd <nisammohd514@gmail.com>
2026-09-18 14:52:40 +0530

    thunk created to delete product from server

---
+++ src/AdminLayout/redux/thunks/deleteProductThunk.js
@@ -0,0 +1,18 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../../services/api";
+
+export const deleteProduct = createAsyncThunk(
+  "adminProducts/deleteProduct",
+  async (productId, { rejectWithValue }) => {
+    try {
+      const res = await api.delete(`/products/${productId}`);
+      return productId;
+    } catch (err){
+        return rejectWithValue(
+            error.response?.data?.message || 
+            err.message ||
+            "Failed attempt to Delete user"
+        )
+    }
+  },
+);
